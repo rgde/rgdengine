7f9da73631263523746c50bf70901673da3f1422
denis.ovod <denis.ovod@1fd64577-6b28-0410-b610-73a45c22ae85>
2008-06-27 14:28:29 +0000

    iss#000 localization code upgrade

--- branches/rgde2/build/vs8/engine/localization/localization.cpp
+++ branches/rgde2/build/vs8/engine/localization/localization.cpp
@@ -10,32 +10,65 @@ namespace rgde
 		{
 		}
 
-		const std::wstring& localization_table::get_text(const std::string& text_id_string) const
+		const std::wstring& localization_table::get_text(const std::string& id_string) const
 		{
-			string_table_it it = m_table.find(text_id_string);
+			string_table_it it = m_table.find(id_string);
 			return it != m_table.end() ? it->second : m_empty_string;
+		}
+
+		void localization_table::clear()
+		{
+			m_table.clear();
+		}
+
+		void localization_table::add_text(const std::string& id_string, const std::wstring& text)
+		{
+			m_table[id_string] = text;
 		}
 
 		//////////////////////////////////////////////////////////////////////////
 
 		localization_system::localization_system(const std::string& default_locale)
 		{
-			m_empty_table.reset(new base_localization_table());
 			set_locale(default_locale);
 		}
 
 		void localization_system::set_locale(const std::string& new_locale)
 		{
 			m_current_locale = new_locale;
+			m_current_tables = m_tables[m_current_locale];
 		}
 
-		localization_table_ptr localization_system::get_table(const std::string& table_name)
+		localization_table_ptr localization_system::get_table(const std::string& locale, const std::string& table_id)
 		{
-			return m_empty_table;
+			loc_tables_iter it = m_tables.find(locale);
+			
+			if (it != m_tables.end())
+			{
+				return it->second[table_id];
+			}
+			else
+				return m_empty_table;
 		}
 
-		void localization_system::reload_locale()
+		localization_table_ptr localization_system::add_table(const localization_table_ptr& table, const std::string& locale,
+															  const std::string& table_id )
 		{
+			m_tables[locale][table_id] = table;
+			m_current_tables = m_tables[m_current_locale];
+			return table;
+		}
+
+		const std::wstring& localization_system::get_text(const std::string& id_string, const std::string& id_table) const
+		{
+			map_tables_citer it = m_current_tables.find(id_table);
+			if (it != m_current_tables.end() && it->second)
+			{
+				return it->second->get_text(id_string);
+			}
+	
+			const static std::wstring s_empty_string;
+			return s_empty_string;
 		}
 	}
 }
--- branches/rgde2/build/vs8/engine/localization/localization.h
+++ branches/rgde2/build/vs8/engine/localization/localization.h
@@ -9,29 +9,28 @@ namespace rgde
 		struct base_localization_table
 		{
 			virtual ~base_localization_table() {}
-			///virtual const std::wstring& get_text(const std::string& text_id_string) const = 0;
-
-			// temporary
-			virtual const std::wstring& get_text(const std::string& text_id_string) const 
-			{
-				throw core::errors::not_implemented("virtual const std::wstring&  base_localization_table::get_text(const std::string& text_id_string) const");
-			}
+			virtual const std::wstring& get_text(const std::string& id_string) const = 0;
 		};
 
 		typedef boost::shared_ptr<base_localization_table> localization_table_ptr;
+
+		
 
 		// любая подсистема движка, которой нужно иметь дело с языковой информацией
 		// должна содержать в себе такую таблицу
 		class localization_table : public base_localization_table
 		{
-		public:
 			typedef std::map<std::string, std::wstring> string_table;
 			typedef string_table::const_iterator string_table_it;
-
+		public:
 			localization_table();
 
 			// вернет пустую строку если id не найден, альтернатива - вернуть text_id_string
-			const std::wstring& get_text(const std::string& text_id_string) const;
+			const std::wstring& get_text(const std::string& id_string) const;
+			
+			void add_text(const std::string& id_string, const std::wstring& text);
+
+			void clear();
 
 		protected:
 			string_table m_table;
@@ -47,17 +46,23 @@ namespace rgde
 			const std::string& get_locale() const {return m_current_locale;}
 			void set_locale(const std::string& new_locale);
 
-			localization_table_ptr get_table(const std::string& table_name);
+			localization_table_ptr get_table(const std::string& locale, const std::string& table_id);
+			localization_table_ptr add_table(const localization_table_ptr& table, const std::string& locale,
+											 const std::string& table_id);
+
+			const std::wstring& get_text(const std::string& table_id, const std::string& id_string) const;
 
 		protected:
-			virtual void reload_locale();
+			typedef std::map<std::string, localization_table_ptr> map_tables;
+			typedef map_tables::const_iterator map_tables_citer;
+			typedef std::map<std::string, typename map_tables> loc_tables;
+			typedef loc_tables::iterator loc_tables_iter;
 
-		protected:
-			typedef std::map<std::string, localization_table_ptr> loc_tables;
 			localization_table_ptr m_empty_table;
 			loc_tables m_tables;
 
 			std::string m_current_locale;
+			map_tables m_current_tables;
 		};
 	}
 }
