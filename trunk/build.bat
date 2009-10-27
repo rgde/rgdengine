27172ce8637c958cbf1dbaa53295ccb2957b27ca
denis.ovod <denis.ovod@1fd64577-6b28-0410-b610-73a45c22ae85>
2009-10-27 17:40:02 +0000

    #000 moved to VS9

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,11 +1,11 @@
 @echo off
-if not defined VS80COMNTOOLS (
-	echo In order to build this project you need MS Visual Studio 2005 to be installed.
+if not defined VS90COMNTOOLS (
+	echo In order to build this project you need MS Visual Studio 2008 to be installed.
 	echo http://msdn.microsoft.com/vstudio/express/downloads/default.aspx
 	exit
 )
 
-call "%VS80COMNTOOLS%\vsvars32.bat" || exit /b 1
+call "%VS90COMNTOOLS%\vsvars32.bat" || exit /b 1
 devenv.com build.sln /rebuild release
 devenv.com build.sln /rebuild debug
 pause
