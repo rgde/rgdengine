fee1c05045caf9dd66e1675302b1b89ee16cf93f
strelkin7 <strelkin7@1fd64577-6b28-0410-b610-73a45c22ae85>
2009-11-06 00:55:33 +0000

    #012 Fixed renderer2d compilation. Updated test project.

--- rgde2-bactcher2d/build/vs8/tests/batcher2d_test/BatcherApplication.cpp
+++ rgde2-bactcher2d/build/vs8/tests/batcher2d_test/BatcherApplication.cpp
@@ -6,6 +6,9 @@
 namespace rgde
 {
 	BatcherApplication::BatcherApplication(int x, int y, int w, int h, const std::wstring& title)
+		:m_active(true),
+		window(math::vec2i(x, y), math::vec2i(w, h), title, 0, WS_BORDER | WS_CAPTION | WS_SYSMENU),
+		m_device(get_handle())
 	{
 	}
 
@@ -13,7 +16,55 @@ namespace rgde
 	{
 	}
 
-	void BatcherApplication::update()
+	void BatcherApplication::run()
 	{
+		while( is_created() )
+		{
+			if( !do_events() && m_active)
+			{
+			}
+		}
+	}
+
+	core::windows::result BatcherApplication::wnd_proc(ushort message, uint wparam, long lparam )
+	{
+		switch (message)
+		{
+		case WM_ACTIVATE:	// Watch For Window Activate Message
+			{
+				if (!HIWORD(wparam))// Check Minimization State
+					m_active = true;
+				else
+					m_active = false;
+
+				return 0;
+			}
+		case WM_KEYDOWN:
+			{
+				return 0;
+			}
+			break;
+		case WM_SIZE:
+			{
+				return 0;
+			}
+			break;
+
+		case WM_MOUSEMOVE:
+			break;
+		}
+		return window::wnd_proc(message, wparam, lparam);
+	}
+
+	bool BatcherApplication::do_events()
+	{
+		MSG msg = {0};
+		if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) )
+		{
+			TranslateMessage( &msg );
+			DispatchMessage( &msg );
+			return true;
+		}
+		return false;
 	}
 }
