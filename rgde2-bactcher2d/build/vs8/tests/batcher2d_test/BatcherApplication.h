636bd62bb80d50ebc6549c28b66c390ed39499e4
denis.ovod <denis.ovod@1fd64577-6b28-0410-b610-73a45c22ae85>
2009-11-26 08:05:38 +0000

    -fixed linkings (missed dependency from renderer_2d) -some code improvements (please check it)

--- rgde2-bactcher2d/build/vs8/tests/batcher2d_test/BatcherApplication.h
+++ rgde2-bactcher2d/build/vs8/tests/batcher2d_test/BatcherApplication.h
@@ -5,20 +5,27 @@ namespace rgde
 	class BatcherApplication : public core::windows::window
 	{
 	public:
+		typedef render::primitives_2d::sprite_desc sprite_desc;
+
 		BatcherApplication(int x, int y, int w, int h, const std::wstring& title);
 		~BatcherApplication();
 
 		void run();
-		void add_sprite(render::primitives_2d::sprite_desc& sprite);
-	protected:
-		virtual core::windows::result wnd_proc(ushort message, uint wparam, long lparam );
+		void add_sprite(const sprite_desc& sprite);
+
+	protected:		
 		bool do_events();
 		void render_frame();
 		void init_render_data();
+
+		core::windows::result wnd_proc(ushort message, uint wparam, long lparam );
+
 	private:
 		bool m_active;
 		render::device m_device;
-		render::renderer_2d* m_batcher;
-		std::vector<render::primitives_2d::sprite_desc*> sprites;
+		render::renderer_2d m_batcher;
+		
+		// not used:
+		//std::vector<render::primitives_2d::sprite_desc*> sprites; 
 	};
 }
