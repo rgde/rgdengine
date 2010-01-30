3761c326578676eb6692e74f34d492e5ad2e253e
denis.ovod <denis.ovod@1fd64577-6b28-0410-b610-73a45c22ae85>
2010-01-30 10:35:11 +0000

    -gui fix

--- branches/rgde2/build/vs9/guilib/src/size.cpp
+++ branches/rgde2/build/vs9/guilib/src/size.cpp
@@ -26,16 +26,14 @@ namespace gui
 	Size StringToSize(const std::string& str)
 	{
 		Size out;
-
-		sscanf(str.c_str(), "%d %d", &out.width, &out.height);
-
+		sscanf(str.c_str(), "%f %f", &out.width, &out.height);
 		return out;
 	}
 
 	std::string SizeToString(const Size& val)
 	{
 		char buff[128] = {0};
-		_snprintf(buff, sizeof (buff), "%d %d", val.width, val.height);
+		_snprintf(buff, sizeof (buff), "%f %f", val.width, val.height);
 		return std::string(buff);
 	}
 }
