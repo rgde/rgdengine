558278aa2e4a231de43e7df426003da53c55daea
enumerous <enumerous@1fd64577-6b28-0410-b610-73a45c22ae85>
2010-02-01 20:58:32 +0000

    test

--- branches/rgde2/build/vs9/tests/guitest/Xml2Test.cpp
+++ branches/rgde2/build/vs9/tests/guitest/Xml2Test.cpp
@@ -8,8 +8,8 @@ Xml2Test::Xml2Test(int x, int y, int w, int h, const std::wstring& title)
 	//m_filename = "guitest\\hud.xml";
 	//m_filename = "guitest\\sampleframetest_orig.xml";
 	//m_filename = "guitest\\sampleframe.xml";
-	m_filename = "guitest\\testfr.xml";	
-	m_filename = "guitest\\testfr2.xml";	
+	m_filename = "guitest\\test.xml";	
+	//m_filename = "guitest\\testfr2.xml";	
 }
 
 Xml2Test::~Xml2Test(void)
@@ -19,14 +19,7 @@ Xml2Test::~Xml2Test(void)
 void Xml2Test::createGUISystem()
 {
 	ui_test_application::createGUISystem();
-	//load(m_filename);
-
-	load("guitest\\testfr.xml");	
-	load("guitest\\testfr2.xml");
-	load("guitest\\testfr3.xml");
-	load("guitest\\testfr4.xml");	
-	load("guitest\\testfr5.xml");
-	load("guitest\\testfr6.xml");
+	load(m_filename);
 }
 
 void Xml2Test::resetGUISystem()
