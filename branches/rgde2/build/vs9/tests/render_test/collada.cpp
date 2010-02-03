7e50856b0d4fe1474b7b997b625b3000f8efabf3
denis.ovod <denis.ovod@1fd64577-6b28-0410-b610-73a45c22ae85>
2010-02-03 10:45:31 +0000

    -draft

--- branches/rgde2/build/vs9/tests/render_test/collada.cpp
+++ branches/rgde2/build/vs9/tests/render_test/collada.cpp
@@ -70,6 +70,39 @@ namespace collada
 
 					const char* str_data = array_node.first_child().value();
 
+					//bool res = get_source_accessor_params( element, accessor_count, accessor_stride );
+
+					//TiXmlHandle source_handle = TiXmlHandle( const_cast<TiXmlElement*>(element) );
+					//const TiXmlElement* accessor_element = source_handle.FirstChild( "technique_common" ).FirstChild( "accessor" ).ToElement();
+					//if( accessor_element )
+					//{
+					//	accessor_element->Attribute( count_attribute, &count );
+					//	accessor_element->Attribute( stride_attribute, &stride );
+					//	return true;
+					//}
+					//else
+					//	return false;
+
+					/*const TiXmlElement* float_array_element = element->FirstChildElement( "float_array" );*/
+
+					//const char* text = float_array_element->GetText();
+					//char* buf = const_cast<char*>(text);
+					//for( int i = 0; i < accessor_count; ++i )
+					//{
+					//	vector3& v = container[i];
+					//	v[0] = static_cast<float>(strtod(str_data,&str_data));
+					//	v[1] = static_cast<float>(strtod(str_data,&str_data));
+					//	v[2] = static_cast<float>(strtod(str_data,&str_data));
+					//}		
+
+					//const char* text = p_element->GetText();
+					//char* buf = const_cast<char*>(text);
+
+					//while( *buf )
+					//{
+					//	t.indexes_.push_back( strtoul(buf, &buf, 10) );
+					//}
+
 					__asm nop;
 				}
 
@@ -84,4 +117,39 @@ namespace collada
 		__asm nop;
 	}
 
+	//mesh_ptr collada::load_mesh( const TiXmlElement* element, const std::string& geometry_id )
+	//{
+	//	const TiXmlElement* source_element = element->FirstChildElement( "source" );
+
+	//	mesh_ptr the_mesh( new mesh() );
+
+	//	for( ; source_element; source_element = source_element->NextSiblingElement("source") )
+	//	{
+	//		string id = GetStringAttribute( source_element, _id );
+	//		id = id.substr( geometry_id.size() + 1 );
+
+	//		if( trace_)
+	//			std::cout << id <<std::endl;
+
+	//		if( id == "positions" )
+	//			load_source( source_element, the_mesh->positions_array_ );
+	//		else
+	//			if( id == "normals" )
+	//				load_source( source_element, the_mesh->normals_array_ );
+	//			else
+	//				if( id == "map-channel1" )
+	//					load_source( source_element, the_mesh->map_channel1_array_ );
+	//				else
+	//					if( id == "map-channel1-tangents" )
+	//						load_source( source_element, the_mesh->map_channel1_tangents_array_ );
+	//					else
+	//						if( id == "map-channel1-binormals" )
+	//							load_source( source_element, the_mesh->map_channel1_binormals_array_ );
+	//	}
+
+	//	const TiXmlElement* triangles_element = element->FirstChildElement( "triangles" );
+	//	load_triangles( triangles_element, *the_mesh->triangles_.get() );
+
+	//	return the_mesh;
+	//}
 }
